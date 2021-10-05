8d25eee0292e397254d96db8660c13397688375e
mohneesh20 <66146641+mohneesh20@users.noreply.github.com>
2021-10-05 20:44:38 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const mongoose=require('mongoose');
 const cors=require('cors');
 // const path=require('path');
 require('dotenv').config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 const app=express();
 app.use(cors());
 app.use(express.json());
@@ -18,6 +24,12 @@ mongoose.connect(process.env.DB_URI,{
 }).catch((err)=>{
     console.log(err);
 });
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 app.listen(process.env.PORT,()=>{
     console.log('SERVER STARTED');
 });
